1fa29dd1cfb59c565117793b3331e4e1c8b56251
alexriabtsev <alex.riabtsev@gmail.com>
2019-09-25 21:20:29 +0300

    added right footer

--- htdocs/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ htdocs/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -12,3 +12,12 @@
 <div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
 </div><!-- .site-info -->
+<div class="btn_mob	phone">
+	Call us: <a href="tel:0987654321">0987654321</a>
+</div>
+<div class="btn_mob	email">
+	Email: <a href="mailto:testdomain@mail.to">testdomain@mail.to</a>
+</div>
+<div class="btn_mob	popup">
+	<a href="mailto:testdomain@mail.to">Contact us</a>
+</div>
